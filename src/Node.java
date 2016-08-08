6cad1234d485970f9986f02b19c874a90ad0e3fc
Sebastián Hernández <seba.hg@me.com>
2016-08-08 17:15:13 -0400

    unignoring things

---
+++ src/Node.java
@@ -0,0 +1,18 @@
+import javafx.util.Pair;
+
+import java.util.List;
+
+/**
+ * Created by sebito on 08-08-16.
+ */
+public class Node {
+    List<Node> neighbors;
+    float lat;
+    float lon;
+
+    public static List<Node> buildCompleteGraph(List<Pair<Double, Double>> points){
+        for (Pair pair: points){
+
+        }
+    }
+}
